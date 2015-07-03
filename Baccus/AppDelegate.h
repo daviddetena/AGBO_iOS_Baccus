2e290bdb521108566b4f809df8a56ad92e84c253
David de Tena <daviddetenacalvo@gmail.com>
2015-07-03 19:59:58 +0200

    Initial Commit

---
+++ Baccus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baccus
+//
+//  Created by David de Tena on 03/07/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
